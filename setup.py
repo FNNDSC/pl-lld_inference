76645fb4f1a10a4595f01d677d5c1668edb2925c
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-03-24 13:43:27 -0400

    Bump to pull latest pflog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name             = 'lld_inference',
-    version          = '2.2.8',
+    version          = '2.2.10',
     description      = 'An app to run LLD inference',
     author           = 'FNNDSC',
     author_email     = 'dev@babyMRI.org',
