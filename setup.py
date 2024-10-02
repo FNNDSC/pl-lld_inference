451dd2b35bcf412744bfab9a4cdaa98156f61edd
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2024-10-02 11:21:43 -0400

    updated sequence in build steps

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name             = 'lld_inference',
-    version          = '2.2.13',
+    version          = '2.2.14',
     description      = 'An app to run LLD inference',
     author           = 'FNNDSC',
     author_email     = 'dev@babyMRI.org',
