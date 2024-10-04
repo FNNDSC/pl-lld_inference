e01ca7e77b2bf8366261a59f12a166f489b70224
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2024-10-04 12:03:23 -0400

    fix compute name to NERC

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name             = 'lld_inference',
-    version          = '2.2.21',
+    version          = '2.2.22',
     description      = 'An app to run LLD inference',
     author           = 'FNNDSC',
     author_email     = 'dev@babyMRI.org',
